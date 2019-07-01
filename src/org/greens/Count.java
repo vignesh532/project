bcca98238fe33c270426eff0088983a0c0d72c10
vignesh532 <52267053+vignesh532@users.noreply.github.com>
2019-07-01 12:12:14 +0530

    Update Count.java

--- src/org/greens/Count.java
+++ src/org/greens/Count.java
@@ -1,11 +1,25 @@
 package org.greens;
 
+import java.awt.AWTException;
+
 public class Count {
-	public static void main(String[] args) {
-		
 	
-	 String str = "Welcome To Java class @123"; 
-     int upper = 0, lower = 0, number = 0, special = 0; 
+	 static int k;
+	 	
+	 	static int a[] = { 1,2,3,4,5,6,7,8,9,10};
+
+		
+	public static void main(String[] args) throws AWTException {
+		
+		 String str = "Welcome To Java class @123"; 
+	     int upper = 0, lower = 0, number = 0, special = 0; 
+ 		
+ 		
+ 		for (int i = 0; i < a.length; i++) {
+ 			
+ 			k += a[i];
+ 			
+ 		}
 
      for(int i = 0; i < str.length(); i++) 
      { 
